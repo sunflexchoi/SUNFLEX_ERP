e592390544725417c485baf01c5db4bf5495e54c
sunflexchoi <sunflex.choi@gmail.com>
2026-08-19 14:43:56 +0900

    Update AppContext.tsx

--- context/AppContext.tsx
+++ context/AppContext.tsx
@@ -6,12 +6,10 @@ const AppContext = createContext<any>(null);
 export function AppProvider({ children }: { children: React.ReactNode }) {
   const [isCEO, setIsCEO] = useState(false);
   const [isCollapsed, setIsCollapsed] = useState(false);
-
   return (
     <AppContext.Provider value={{ isCEO, setIsCEO, isCollapsed, setIsCollapsed }}>
       {children}
     </AppContext.Provider>
   );
 }
-
 export const useApp = () => useContext(AppContext);
